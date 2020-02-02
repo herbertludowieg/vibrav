f21f8b2e1bffaf793264dae81dd61ba48b29c2d7
Herbert Ludowieg <herbertl@buffalo.edu>
2020-02-02 00:23:38 -0500

    Updating init file

---
+++ vibrav/__init__.py
@@ -0,0 +1,7 @@
+from .vibronic import *
+
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_version__ = versions['full-revisionid']
+del get_versions, versions
