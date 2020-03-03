0d1f5b07b9d155d31eb7880b01c70d7bef8eef79
Herbert Ludowieg <herbertl@buffalo.edu>
2020-03-03 16:53:05 -0500

    Updating test

--- vibrav/core/tests/test_config.py
+++ vibrav/core/tests/test_config.py
@@ -7,8 +7,10 @@ def test_config():
                  'number_of_states': (tuple, int), 'number_of_nuclei': int,
                  'number_of_modes': int, 'zero_order_file': str,
                  'oscillator_spin_states': int}
-    default= {'sf_energies_file': None, 'so_energies_file': None, 'angmom_file': 'angmom',
-              'dipole_file': 'dipole', 'spin_file': 'spin', 'quadrupole_file': 'quadrupole'}
+    default= {'sf_energies_file': ('', str), 'so_energies_file': ('', str),
+              'angmom_file': ('angmom', str), 'dipole_file': ('dipole', str),
+              'spin_file': ('spin', str), 'quadrupole_file': ('quadrupole', str),
+              'degen_delta': (1e-5, float)}
     config = Config.open_config(resource('molcas-ucl6-2minus-vibronic-config'), required=required,
                                 defaults=default)
     base = {'number_of_multiplicity': 2, 'spin_multiplicity': (3, 1), 'number_of_states': (42, 49),
@@ -16,7 +18,7 @@ def test_config():
             'oscillator_spin_states': 91, 'delta_file': 'delta.dat',
             'reduced_mass_file': 'redmass.dat', 'frequency_file': 'freq.dat',
             'sf_energies_file': 'energies-SF.txt', 'so_energies_file': 'energies.txt',
-            'zero_order_file': 'ucl-rassi.out'}
+            'zero_order_file': 'ucl-rassi.out', 'degen_delta': 1e-5}
     for key, val in base.items():
         assert val == config[key]
 
