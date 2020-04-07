7017e36e66c062af69561329ff51a48628cb03fa
Herbert D Ludowieg <hbr691@gmail.com>
2020-04-07 11:40:42 -0400

    Updated adf test

--- vibrav/adf/tests/test_output.py
+++ vibrav/adf/tests/test_output.py
@@ -6,9 +6,9 @@ import pytest
 import numpy as np
 import pandas as pd
 
-@pytest.fixture(scope="module")
-def editor():
-    comp = resource("adf-ch4-freq.t21.ascii.xz")
+@pytest.fixture
+def editor(res_file):
+    comp = resource(res_file)
     decomp = comp.split(os.sep)[-1][:-3]
     with open(decomp, 'wb') as new_file, lzma.LZMAFile(comp, 'rb') as file:
         for data in iter(lambda : file.read(100 *1024), b''):
@@ -17,16 +17,21 @@ def editor():
     yield editor
     os.remove(decomp)
 
-def test_atom(editor):
-    data = pd.read_csv(resource('adf-ch4-atoms.csv.xz'), compression='xz', header=0,
-                       index_col=False)
+atom_params = [("adf-ch4-freq.t21.ascii.xz", "adf-ch4-atom.csv.xz"),
+               ("adf-ethane-ts-freq.t21.ascii.xz", "adf-ethane-atom.csv.xz")]
+freq_params = [("adf-ch4-freq.t21.ascii.xz", "adf-ch4-frequency.csv.xz"),
+               ("adf-ethane-ts-freq.t21.ascii.xz", "adf-ethane-frequency.csv.xz")]
+
+@pytest.mark.parametrize("res_file,test_file", atom_params)
+def test_atom(editor, test_file):
+    data = pd.read_csv(resource(test_file), compression='xz', header=0, index_col=False)
     editor.parse_atom()
     cols = ['x', 'y', 'z', 'Z']
     assert np.allclose(data[cols].values, editor.atom[cols].values)
 
-def test_frequency(editor):
-    data = pd.read_csv(resource('adf-ch4-frequencies.csv.xz'), compression='xz', header=0,
-                       index_col=False)
+@pytest.mark.parametrize("res_file,test_file", freq_params)
+def test_frequency(editor, test_file):
+    data = pd.read_csv(resource(test_file), compression='xz', header=0, index_col=False)
     editor.parse_frequency()
     cols = ['dx', 'dy', 'dz', 'frequency']
     assert np.allclose(data[cols].values, editor.frequency[cols].values)
