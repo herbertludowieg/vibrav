3de5b0db1330e5e7392f7198047261cfc31b264b
Herbert D Ludowieg <hbr691@gmail.com>
2020-05-13 16:16:19 -0400

    Adding some parsers

--- vibrav/adf/output.py
+++ vibrav/adf/output.py
@@ -1,6 +1,8 @@
 from exa.core import Editor, TypedMeta
 from exa.util.units import Length, Mass, Energy
-from exatomic.core import Atom, Frequency
+from exatomic.core.atom import Atom, Frequency
+from exatomic.core.gradient import Gradient
+from exatomic.core.tensor import JCoupling
 from exatomic.base import z2sym, sym2isomass
 import numpy as np
 import pandas as pd
@@ -10,14 +12,44 @@ import warnings
 class Tape21Meta(TypedMeta):
     atom = Atom
     frequency = Frequency
+    gradient = Gradient
+    j_coupling = JCoupling
 
 class Tape21(six.with_metaclass(Tape21Meta, Editor)):
     '''
     Parser for ADF Tape21 that have been converted to an ASCII file with
     their dmpkf utility.
     '''
+
+    @staticmethod
+    def rmass_mwc(data, symbol):
+        cols = ['dx', 'dy', 'dz']
+        mapper = sym2isomass(symbol)
+        mass = list(map(mapper.get, symbol))
+        mass = np.repeat(mass, 3).astype(float)
+        mass = mass.reshape(data[cols].shape)
+        disps = data[cols].values
+        r_mass = np.sum(np.square(disps)/mass)
+        r_mass = 1/r_mass
+        return r_mass
+
+    @staticmethod
+    def rmass_cart(data, symbol):
+        cols = ['dx', 'dy', 'dz']
+        mapper = sym2isomass(symbol)
+        mass = list(map(mapper.get, symbol))
+        mass = np.repeat(mass, 3).astype(float)
+        mass = mass.reshape(data[cols].shape)
+        disps = data[cols].values
+        norms = np.linalg.norm(disps*np.sqrt(mass))
+        norms = 1/norms
+        disps *= norms
+        r_mass = np.sum(np.square(disps))
+        r_mass = 1/r_mass
+        return r_mass
+
     def _intme(self, fitem, idx=0):
-        return int(self[fitem[idx]].split()[-1])
+        return int(self[fitem[idx]+1].split()[0])
 
     def _dfme(self, fitem, dim, idx=0):
         start = fitem[idx] + 2
@@ -25,7 +57,7 @@ class Tape21(six.with_metaclass(Tape21Meta, Editor)):
         stop = np.ceil(start + dim / col).astype(np.int64)
         return self.pandas_dataframe(start, stop, col).stack().values
 
-    def parse_frequency(self):
+    def parse_frequency(self, cart=True):
         '''
         ADF frequency parser.
 
@@ -38,9 +70,22 @@ class Tape21(six.with_metaclass(Tape21Meta, Editor)):
         '''
         # search flags
         _renorm = "NormalModes_RAW"
-        _recartmodes = "Normalmodes"
+        _recartnorm = "Normalmodes"
         _refreq = "Frequencies"
-        found = self.find(_refreq, _renorm, _recartmodes, keys_only=True)
+        _refreqexc = r"\bFrequencies\b"
+        _rekey = r"\bFreq\b"
+        found = self.find(_refreq, _renorm, _recartnorm, keys_only=True)
+        key = self.regex(_rekey, _refreqexc, keys_only=True)
+        # need to do this to ensure that we only match the data in the Freq block
+        found_freq = []
+        for k in key[_rekey]:
+            for f in found[_refreq]:
+                if f-1 == k:
+                    found_freq.append(f)
+                    break
+        if not found_freq:
+            return
+        found[_refreq] = found_freq
         if not found[_refreq]:
             return
         if not hasattr(self, 'atom'):
@@ -58,20 +103,18 @@ class Tape21(six.with_metaclass(Tape21Meta, Editor)):
         freq = freq[low]
         nmodes = freq.shape[0]
         freq = np.repeat(freq, nat)
-        if found[_renorm]:
+        if found[_renorm] and not cart:
             # get the mass-weighted normal modes
             ndisps = int(self[found[_renorm][0]+1].split()[0])
             normalmodes = self._dfme(np.array(found[_renorm]), ndisps, idx=0)
             calc_rmass = True
-        elif found[_recartnorm] and not found[_renorm]:
+        elif found[_recartnorm] and cart:
             # get the non-mass-weighted normal modes and toss warning
-            text = "Mass-weighted normal modes could not be found. If you are " \
-                  +"performing vibrational analysis they must be mass-weighted " \
-                  +"normal modes."
-            warnings.warn(text, Warning)
-            ndisps = int(self[found[_recartmodes][0]+1].split()[0])
-            normalmodes = self._dfme(np.array(found[_recartmodes]), ndisps, idx=0)
-            calc_rmass = False
+            ndisps = int(self[found[_recartnorm][0]+1].split()[0])
+            normalmodes = self._dfme(np.array(found[_recartnorm]), ndisps, idx=0)
+            calc_rmass = True
+        else:
+            raise Exception("Something went wrong")
         # get the vibrational modes in the three cartesian directions
         # the loop is neede in case there are any negative modes
         # because then the normal mode displacements for the negative mode
@@ -95,16 +138,17 @@ class Tape21(six.with_metaclass(Tape21Meta, Editor)):
         mass = symbol.map(mapper).astype(float).values
         symbol = np.tile(self.atom['symbol'], nmodes)
         mass = np.repeat(mass, 3)
+        print(dx.shape, dy.shape, dz.shape, freq.shape, freqdx.shape)
         # put the data together
         df = pd.DataFrame({'dx': dx, 'dy': dy, 'dz': dz, 'frequency': freq,
                            'freqdx': freqdx})
         cols = ['dx', 'dy', 'dz']
         # calculate the reduced masses
-        if calc_rmass:
-            r_mass = df.groupby(['freqdx']).apply(lambda x: 1 \
-                                                        / np.sum(np.square(x[cols].values.flatten()) \
-                                                        * 1/mass)).values
-            df['r_mass'] = np.repeat(r_mass, nat)
+        if not cart:
+            r_mass = df.groupby(['freqdx']).apply(self.rmass_mwc, self.atom['symbol']).values
+        else:
+            r_mass = df.groupby(['freqdx']).apply(self.rmass_cart, self.atom['symbol']).values
+        df['r_mass'] = np.repeat(r_mass, nat)
         df['symbol'] = symbol
         df['label'] = label
         df['ir_int'] = 0
@@ -150,4 +194,61 @@ class Tape21(six.with_metaclass(Tape21Meta, Editor)):
         df = pd.DataFrame.from_dict({'symbol': symbol, 'set': set, 'label': set, 'x': x, 'y': y,
                                      'z': z, 'Z': zinput, 'frame': 0})
         self.atom = df
+
+    def parse_gradient(self):
+        # search flags
+        _reinpgrad = "Gradients_InputOrder"
+        _refrggrad = "Gradients_CART"
+        found = self.find(_reinpgrad, _refrggrad, keys_only=True)
+        if not found[_reinpgrad]:
+            raise NotImplementedError("Have not implemented reading the re-ordered gradients.")
+        # get the gradients
+        ngrad = self._intme(np.array(found[_reinpgrad]))
+        grad = self._dfme(np.array(found[_reinpgrad]), ngrad)
+        x = grad[::3]
+        y = grad[1::3]
+        z = grad[2::3]
+        if not hasattr(self, 'atom'):
+            self.parse_atom()
+        symbol = self.atom['symbol'].values
+        Z = self.atom['Z'].values.astype(int)
+        atom = list(range(len(x)))
+        df = pd.DataFrame.from_dict({'Z': Z, 'atom': atom, 'fx': x, 'fy': y, 'fz': z, 'symbol': symbol,
+                                     'frame': 0})
+        df = df[['atom', 'Z', 'fx', 'fy', 'fz', 'symbol', 'frame']]
+        #for u in ['fx', 'fy', 'fz']: df[u] *= 1./Length['Angstrom', 'au']
+        self.gradient = df
+
+    def parse_nmr_shielding(self):
+        raise NotImplementedError("Coming soon!!")
+
+    def parse_j_coupling(self):
+        _reiso = "NMR Coupling J const InputOrder"
+        _retensor = "NMR Coupling J tens InputOrder"
+        found = self.find(_reiso, _retensor, keys_only=True)
+        if not found[_reiso]:
+            return
+        if not hasattr(self, 'atom'):
+            self.parse_atom()
+        ncoupl = self._intme(found[_reiso])
+        natom = np.sqrt(ncoupl)
+        coupling = self._dfme(found[_reiso], ncoupl)
+        ntens = self._intme(found[_retensor])
+        tensor = self._dfme(found[_retensor], ntens)
+        requested = np.where(coupling != 0)[0]
+        tensor = tensor.reshape(ncoupl, 9)[requested]
+        cols = ['xx', 'xy', 'xz', 'yx', 'yy', 'yz', 'zx', 'zy', 'zz']
+        df = pd.DataFrame(tensor, columns=cols)
+        atoms = np.transpose(list(map(lambda x: divmod(x, natom), requested)))
+        df['isotropic'] = coupling[coupling != 0]
+        df['atom'] = atoms[0].astype(int)
+        symbols = self.atom['symbol'].values
+        if len(symbols) > natom:
+            raise NotImplementedError("Cannot deal with more than one atom frame.")
+        df['symbol'] = list(map(lambda x: symbols[x], df['atom'].values))
+        df['pt_atom'] = atoms[1].astype(int)
+        df['pt_symbol'] = list(map(lambda x: symbols[x], df['pt_atom'].values))
+        df['label'] = 'j_coupling'
+        df['frame'] = 0
+        self.j_coupling = df
 
